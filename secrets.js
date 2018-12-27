106df18d6ce6dabbcfa4e0d925351e63e00603a5
Jing Lu <jl201835@gmail.com>
2018-12-27 15:38:26 -0500

    test

---
+++ secrets.js
@@ -0,0 +1,4 @@
+process.env.GOOGLE_CLIENT_ID =
+  '418712199127-gksb5cvsdjhel8h8vird47qojt89ltpk.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = 'MdIwyQnjtG1QBGsOSzjewblZ'
+process.env.GOOGLE_CALLBACK = 'auth/google/callback'
